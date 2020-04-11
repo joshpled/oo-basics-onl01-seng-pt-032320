ce10572681f256d5a6f2c78605e1bcd860b81fdf
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-11 02:42:17 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,10 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition, :cobble
-  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand, :cobble
   def initialize(brand)
    @brand = brand
  end
+ def cobble
+   puts "Your shoe is as good as new!"
+ end 
 end
