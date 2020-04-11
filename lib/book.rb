cda3099171ceac1bcba266407a3d5535abc6ec64
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-11 02:21:12 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,9 @@
 class Book
-  def initialize(title,author)
+  def initialize(title)
     @title = title
-    @author = author
+end
+def author=(author)
+  @author = author
 end
 def author
   @author
